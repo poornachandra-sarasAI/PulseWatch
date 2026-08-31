7a931c7e2406dfff4604669fdca2dcc38be3ec04
R Mohan Poornachandra <mohan.poornachandra01@sarasai.org>
2026-08-31 16:26:29 +0530

    Delete frontend/.next directory

--- frontend/.next/dev/server/chunks/ssr/_next-internal_server_app_monitors_page_actions_0_aosx9.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-"[project]/.next-internal/server/app/monitors/page/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
-
-}),
-];
-
-//# sourceMappingURL=_next-internal_server_app_monitors_page_actions_0_aosx9.js.map
--- frontend/.next/standalone/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"L2tVLK3Lq/XQUD/ybbjTugu7Zp2hFcCzm8pAqgzA6NQ=\"\n}"
